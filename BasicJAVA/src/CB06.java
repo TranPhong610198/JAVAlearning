76c21df1afbc61dab19b641e67d029d4e327ab5b
Moriarty <tphong610198@gmail.com>
2024-01-08 15:58:23 +0700

    commited by Moriarty

---
+++ BasicJAVA/src/CB06.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class CB06 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int r, d;
+        r = input.nextInt();
+        d = input.nextInt();
+        System.out.println((r+d)*2);
+        System.out.println(r*d);
+    }
+}
---
+++ BasicJAVA/src/CB07.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class CB07 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        float pi = 3.140f;
+        float r = input.nextFloat();
+        System.out.println(2*pi*r +" "+ pi*r*r);
+    }
+}
